036e8e981bdb6aa96f75c2523cd8b6f93f9457d7
dthema <gleb.info2102@gmail.com>
2023-12-16 21:17:16 +0300

    feat: voice commands

--- js/all-pages.js
+++ js/all-pages.js
@@ -8,7 +8,59 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    let isListen = false;
+    let annyangScript = document.createElement('script');
+    annyangScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.1/annyang.min.js';
+    document.body.appendChild(annyangScript);
 
+    annyangScript.onload = function() {
+        let speechkittScript = document.createElement('script');
+        speechkittScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/speechkitt.min.js';
+        document.body.appendChild(speechkittScript);
+
+        speechkittScript.onload = function() {
+            initializeAnnyang();
+        };
+    };
+
+    function initializeAnnyang() {
+        if (annyang) {
+            annyang.setLanguage('ru');
+
+            annyang.addCommands({
+                "открой главную": () => navigateTo("index.html"),
+                "открой текущие игры": () => navigateTo("games.html"),
+                "открой соревнования": () => navigateTo("competitions.html"),
+                "открой рейтинг": () => navigateTo("ranking.html"),
+            });
+
+            SpeechKITT.annyang();
+            SpeechKITT.setStylesheet('https://cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat.css');
+            SpeechKITT.vroom();
+
+            console.log('Annyang is ready!');
+        } else {
+            console.error('Annyang not available.');
+        }
+    }
+
+    function navigateTo(url) {
+        window.location.href = url;
+    }
+
+    function handleKeyPress(event) {
+        // Проверка, что нажата именно клавиша 'L'
+        if (event.key === 'L' || event.key === 'l') {
+            if (isListen) {
+                annyang.abort();
+            } else {
+                annyang.start();
+            }
+            isListen = !isListen;
+        }
+    }
+
+    document.addEventListener('keydown', handleKeyPress);
 });
 
 (function() {
